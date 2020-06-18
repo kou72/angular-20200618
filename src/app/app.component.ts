7160503113122575fba57e55bfa1634f4a43b2b2
kou72 <genpokopokopoko@gmail.com>
2020-06-18 21:06:11 +0900

    step 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { todoList } from './todoList';
+
+import { Todo } from './todo';
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  title = "test";
+  title = 'My todo-list';
+  todoList: Todo[] = [...todoList];
 }
